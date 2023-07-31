b8bfd2bca9b0660cc76ce9fbbee085a45b216d7d
Jakub Nicewicz <njakub94@gmail.com>
2023-07-31 21:09:05 +0100

    change dark mode colors and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
   return (
     <div>
       <main className="flex flex-col items-center justify-between min-h-screen p-24">
-        <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex"></div>
+        <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+            Full English Finder
+          </h1>
+          <h3>Find yourself a proper Full English!</h3>
+        </div>
       </main>
     </div>
   );
